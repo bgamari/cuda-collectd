c1df7a5e74e0c4d5afd947c9cb8a8cdbb0574dbb
Ben Gamari <bgamari.foss@gmail.com>
2013-08-18 12:55:19 -0400

    Various typos and values need to be a list

--- cuda_collectd.py
+++ cuda_collectd.py
@@ -15,13 +15,13 @@ def read(data=None):
                 vl.plugin_instance = 'cuda-%s' % (gpu.attrib['id'])
 
                 vl.dispatch(type='fanspeed',
-                            values=float(gpu.find('fan_speed').text.split()[0]))
+                            values=[float(gpu.find('fan_speed').text.split()[0])])
 
-                vl.display(type='temperature',
-                           values=float(gpu.find('temperature/gpu_temp').text.split()[0]))
+                vl.dispatch(type='temperature',
+                            values=[float(gpu.find('temperature/gpu_temp').text.split()[0])])
 
                 vl.dispatch(type='memory', type_instance='used',
-                            values=float(1e6 * gpu.find('memory_usage/used').text.split()[0]))
+                            values=[float(1e6 * gpu.find('memory_usage/used').text.split()[0])])
 
 collectd.register_read(read)
 
